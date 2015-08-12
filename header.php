59bbb72c81e309ce60e3ca02a40265f1f39e67ca
Joanna Stecewicz <joanna.stecewicz@gmail.com>
2015-08-12 19:04:25 -0400

    nav

--- header.php
+++ header.php
@@ -4,10 +4,15 @@
 	<?php // Load Meta ?>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Meta tag from responsive menu plugin -->
+  <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' />
+
   <title><?php  wp_title('|', true, 'right'); ?></title>
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
+
   <?php // Load our CSS ?>
+  <link href='http://fonts.googleapis.com/css?family=Abril+Fatface|Lato:400,100,300' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 
   <?php wp_head(); ?>
@@ -17,13 +22,12 @@
 <body <?php body_class(); ?>>
 
 <header>
-  <div class="container">
-    <h1>
+  <div class="container clearfix">
+    <!-- <h1>
       <a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name', 'display' ); ?>" rel="home">
         <?php bloginfo( 'name' ); ?>
       </a>
-    </h1>
-
+    </h1> -->
     <?php wp_nav_menu( array(
       'container' => false,
       'theme_locations' => 'primary'
--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php //index.php is the last resort template, if no other templates match ?>
 <?php get_header(); ?>
-
 <div class="main">
   <div class="container">
 	
     <div class="content">
-    		<h2>HELLO WORLD.</h2>
     		<?php get_template_part( 'loop', 'index' );	?>
     </div> <!--/.content -->
 
